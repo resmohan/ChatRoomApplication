7e34d569e455977bafcef105ddfc55fc1360c95f
resmohan <mohan.re@northeastern.edu>
2023-04-24 11:31:30 -0700

    ProjectUpload

---
+++ ChatRoomApplication/src/test/java/chatRoom/chatRoomApp/ChatClientAppTest.java
@@ -0,0 +1,30 @@
+package chatRoom.chatRoomApp;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ChatClientAppTest {
+
+  private ByteArrayOutputStream outContent;
+  private final PrintStream originalOut = System.out;
+  @BeforeEach
+  void setUp() {
+    outContent = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(outContent));
+  }
+
+  @AfterEach
+  public void restoreStreams() {
+    System.setOut(originalOut);
+  }
+
+  @Test
+  void mainServerNotUp() {
+    ChatClientApp.main(new String[]{"localhost","3400"});
+    Assertions.assertEquals(ChatClientApp.SERVER_NOT_AVAILABLE.replace("\n", "").replace("\r",""), outContent.toString().replace("\n", "").replace("\r",""));
+  }
+}
